2f38ab8420f107f90b8be4b7842f0e77e0c8dc37
stepz <step.zip@mail.ru>
2023-06-13 17:02:11 +0300

    ghbsde

--- test22.py
+++ test22.py
@@ -4,11 +4,11 @@ import time, random, wrap
 world.create_world(800, 600)
 
 v = False
-sze1 = 20
-sze2 = 20
+width = 20
+height = 20
 aim = sprite.add("pacman", 100, 100, "player2", v)
 sprite.set_angle(aim, 0)
-size = sprite.set_size(aim, sze1, sze2)
+size = sprite.set_size(aim, width, height)
 green_tank = sprite.add("battle_city_tanks", 100, 100, "tank_enemy_size1_green1")
 
 
@@ -17,27 +17,32 @@ def tp_tank():
     y_r = random.randint(100, 500)
     x_r = random.randint(100, 700)
     sprite.move_to(green_tank, x_r, y_r)
-
+    height1=sprite.get_height(aim)
+    width1=sprite.get_width(aim)
+    print(width,height,height1,width1)
 
 @wrap.on_key_down(wrap.K_UP)
-def aims(pos_x, pos_y):
-    global sze1, sze2
-    sprite.move_to(aim, pos_x, pos_y)
+def aims():
+    global height,width
+    sprite.set_size(aim,width, height)
     sprite.show(aim)
-    if v == True:
+
+    if width == 20 and height == 20:
+        width = width + 20
+        height = height + 20
+    elif width == 40 and height == 40:
+        width = width + 20
+        height = height + 20
+    elif width == 60 and height == 60:
+        width = width + 20
+        height = height + 20
+
+    elif width==80 and height == 80:
         sprite.hide(aim)
-    if v == False:
-        sprite.set_size(aim, sze1, sze2)
-        sprite.show(aim)
-    if sze1 == 20 and sze2 == 20:
-        sze1 = sze1 + 20
-        sze2 = sze2 + 20
-    if sze1 == 40 and sze2 == 40:
-        sze1 = sze1 + 20
-        sze2 = sze2 + 20
-    if sze1 == 60 and sze2 == 60:
-        sze1 = sze1 + 20
-        sze2 = sze2 + 20
-    if sze1 == 80 and sze2 == 80:
-        sze1 = sze1 + 20
-        sze2 = sze2 + 20
+        width=20
+        height=20
+
+
+@wrap.on_mouse_move
+def dv_aim(pos_x,pos_y):
+    sprite.move_to(aim,pos_x,pos_y)
