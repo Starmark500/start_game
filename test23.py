2ab3f37557fdf198970f28d00da5a590edd4a89f
stepz <step.zip@mail.ru>
2023-07-14 18:00:14 +0300

    14.07.2023

--- test23.py
+++ test23.py
@@ -4,35 +4,69 @@ from wrap import sprite, world, sprite_text, actions
 import time, random, wrap
 
 world.create_world(800, 800)
-world.set_back_color(255,255,10)
+world.set_back_color(255, 255, 10)
+
+turtle = sprite.add("mario-enemies", 100, 100, "turtle_stand")
+mario_b = sprite.add("mario-2-big", 300, 200, "walk1")
+apple = sprite.add("pacman", 500, 600, "item_apple")
 
 
-turtle=sprite.add("mario-enemies",100,100,"turtle_stand")
-mario_b=sprite.add("mario-2-big",300,200,"walk1")
-apple=sprite.add("pacman", 500, 600,"item_apple")
-
-@wrap.on_mouse_down(wrap.BUTTON_WHEELUP,wrap.BUTTON_WHEELDOWN)
-def plus(pos_x,pos_y,button):
-    if sprite.is_collide_point(turtle,pos_x,pos_y):
-        id_object=turtle
-    elif sprite.is_collide_point(mario_b,pos_x,pos_y):
-        id_object=mario_b
-
-    if button==wrap.BUTTON_WHEELUP:
-        change_size=2
+@wrap.on_mouse_down(wrap.BUTTON_WHEELUP, wrap.BUTTON_WHEELDOWN)
+def plus(pos_x, pos_y, button):
+    if sprite.is_collide_point(turtle, pos_x, pos_y):
+        id_object = turtle
+    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
+        id_object = mario_b
+    elif sprite.is_collide_point(apple, pos_x, pos_y):
+        id_object = apple
     else:
-        change_size=-2
-    if sprite.is_collide_point(id_object,pos_x,pos_y):
+        return
+    if button == wrap.BUTTON_WHEELUP:
+        change_size = 2
+    else:
+        change_size = -2
 
-        w_t = sprite.get_width(id_object)
-        h_t = sprite.get_height(id_object)
+    w_t = sprite.get_width_percent(id_object)
+    h_t = sprite.get_height_percent(id_object)
 
-        w_t += change_size
-        h_t += change_size
+    w_t += change_size
+    h_t += change_size
 
-        sprite.set_size(id_object,w_t,h_t)
-
-
+    sprite.set_size_percent(id_object, w_t, h_t)
 
 
+@wrap.on_key_always(wrap.K_RIGHT, wrap.K_LEFT)
+def flip(pos_x, pos_y, keys):
+    if sprite.is_collide_point(turtle, pos_x, pos_y):
+        id_object = turtle
+    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
+        id_object = mario_b
+    elif sprite.is_collide_point(apple, pos_x, pos_y):
+        id_object = apple
+    else:
+        return
 
+    if wrap.K_RIGHT in keys :
+        change_angle = 5
+    else:
+        change_angle = -5
+
+
+    angle_id_object = sprite.get_angle(id_object)
+    angle_id_object += change_angle
+    sprite.set_angle(id_object, angle_id_object)
+
+@wrap.on_key_always(wrap.K_UP, wrap.K_DOWN)
+def flip(pos_x, pos_y, keys):
+    if sprite.is_collide_point(turtle, pos_x, pos_y):
+        id_object = turtle
+    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
+        id_object = mario_b
+    elif sprite.is_collide_point(apple, pos_x, pos_y):
+        id_object = apple
+    else:
+        return
+    if wrap.K_UP in keys :
+        sprite.set_costume_next(id_object)
+    else:
+        sprite.set_costume_prev(id_object)
