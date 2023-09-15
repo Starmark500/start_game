6369b7d69a88236f0ef7a0d957460f36b9ac071f
stepz <step.zip@mail.ru>
2023-09-15 17:59:00 +0300

    15.09.2023

--- test23.py
+++ test23.py
@@ -4,46 +4,36 @@ from wrap import sprite, world, sprite_text, actions
 import time, random, wrap
 
 world.create_world(800, 800)
-world.set_back_color(255, 255, 10)
+# world.set_back_color(, 255, 10)
 
 turtle = sprite.add("mario-enemies", 100, 100, "turtle_stand")
-mario_b = sprite.add("mario-2-big", 300, 200, "walk1")
+mario_b = sprite.add("mario-2-big", 100, 100, "walk1")
 apple = sprite.add("pacman", 500, 600, "item_apple")
 
 
 @wrap.on_mouse_down(wrap.BUTTON_WHEELUP, wrap.BUTTON_WHEELDOWN)
 def plus(pos_x, pos_y, button):
-    if sprite.is_collide_point(turtle, pos_x, pos_y):
-        id_object = turtle
-    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
-        id_object = mario_b
-    elif sprite.is_collide_point(apple, pos_x, pos_y):
-        id_object = apple
-    else:
+    fond = find_object(pos_x, pos_y)
+    if fond == None:
         return
     if button == wrap.BUTTON_WHEELUP:
         change_size = 2
     else:
         change_size = -2
 
-    w_t = sprite.get_width_percent(id_object)
-    h_t = sprite.get_height_percent(id_object)
+    w_t = sprite.get_width_percent(fond)
+    h_t = sprite.get_height_percent(fond)
 
     w_t += change_size
     h_t += change_size
 
-    sprite.set_size_percent(id_object, w_t, h_t)
+    sprite.set_size_percent(fond, w_t, h_t)
 
 
 @wrap.on_key_always(wrap.K_RIGHT, wrap.K_LEFT)
 def flip(pos_x, pos_y, keys):
-    if sprite.is_collide_point(turtle, pos_x, pos_y):
-        id_object = turtle
-    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
-        id_object = mario_b
-    elif sprite.is_collide_point(apple, pos_x, pos_y):
-        id_object = apple
-    else:
+    fond = find_object(pos_x, pos_y)
+    if fond == None:
         return
 
     if wrap.K_RIGHT in keys :
@@ -52,21 +42,28 @@ def flip(pos_x, pos_y, keys):
         change_angle = -5
 
 
-    angle_id_object = sprite.get_angle(id_object)
+    angle_id_object = sprite.get_angle(fond)
     angle_id_object += change_angle
-    sprite.set_angle(id_object, angle_id_object)
+    sprite.set_angle(fond, angle_id_object)
 
 @wrap.on_key_always(wrap.K_UP, wrap.K_DOWN)
 def flip(pos_x, pos_y, keys):
-    if sprite.is_collide_point(turtle, pos_x, pos_y):
-        id_object = turtle
-    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
-        id_object = mario_b
-    elif sprite.is_collide_point(apple, pos_x, pos_y):
-        id_object = apple
-    else:
+    fond=find_object(pos_x,pos_y)
+    if fond == None:
         return
     if wrap.K_UP in keys :
-        sprite.set_costume_next(id_object)
+        sprite.set_costume_next(fond)
     else:
-        sprite.set_costume_prev(id_object)
+        sprite.set_costume_prev(fond)
+
+
+def find_object(pos_x,pos_y):
+    if sprite.is_collide_point(turtle, pos_x, pos_y):
+        return turtle
+    elif sprite.is_collide_point(mario_b, pos_x, pos_y):
+        return mario_b
+    elif sprite.is_collide_point(apple, pos_x, pos_y):
+        return apple
+    else:
+       return
+
