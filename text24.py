27fd1d05f6929b1398889ea4053ea7af328552e6
stepz <step.zip@mail.ru>
2023-09-26 19:32:38 +0300

    26.09.2023

--- text24.py
+++ text24.py
@@ -16,14 +16,32 @@ def move_mario(keys):
         sprite.move_at_angle_dir(mario,5)
     else:
         sprite.set_reverse_x(mario,True)
-        sprite.move_at_angle_dir(mario, 10)
+        sprite.move_at_angle_dir(mario, 5)
 @wrap.on_key_up(wrap.K_SPACE)
 def move_mario(keys,pos_x,pos_y):
+    global m_x,m_y,ang_mario
+    ang_mario=sprite.get_angle(mario)
+    m_x=sprite.get_x(mario)
+    m_y=sprite.get_y(mario)
     sprite.set_costume(mario, "swim3")
 
 @wrap.always(10)
 def go_centr(keys,pos_x,pos_y):
     m_s=sprite.get_costume(mario)
+
     if m_s=="swim3":
-        sprite.set_angle_to_point(mario, pos_x, pos_y)
-        sprite.move_at_angle_dir(mario, 2)
+        mouse_pos = sprite.calc_angle_by_point(mario, pos_x, pos_y)
+        sprite.set_angle(mario,mouse_pos+90)
+        sprite.move_at_angle_point(mario,pos_x,pos_y,7)
+
+        # sprite.set_angle_to_point(mario, pos_x, pos_y)
+        # sprite.move_at_angle_dir(mario, 2)
+
+
+@wrap.on_key_up(wrap.K_DOWN)
+def go_niz(keys,pos_x,pos_y):
+    sprite.set_costume(mario,"stand")
+    sprite.move_at_angle_dir(mario,5)
+    sprite.set_angle_to_point(mario,m_x,m_y)
+    sprite.set_angle(mario,ang_mario)
+    sprite.move_bottom_to(mario, block_top)
